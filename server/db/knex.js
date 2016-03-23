7cfe33dfa80381bdb1326937fb5a41828c1da3a3
Ryan Douglas <ryan@luckydogdigital.com>
2016-03-23 14:56:24 -0600

    changing production environment

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,5 +1,5 @@
 require('dotenv').load();
-var environment = 'development';
+var environment = 'production';
 var config = require('../knexfile')[environment];
 var knex = require('knex')(config);
 module.exports = knex;
